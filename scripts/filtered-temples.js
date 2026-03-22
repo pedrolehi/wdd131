0b0f8089d46eaee7cc6c4da6d845e219c94e50be
Pedro Muniz <pedro@email.com>
2026-03-21 23:33:34 -0300

    Remover código comentado e atualizar datas de dedicação dos templos

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -19,37 +19,40 @@ function toggleActive(element) {
   element.classList.add("active");
 }
 
-document.querySelector('#all').addEventListener('click', () => {
-  toggleActive(document.querySelector('#all'));
-  createTempleCard(temples);
-});
 
-document.querySelector('#old').addEventListener('click', () => {
-  toggleActive(document.querySelector('#old'));
-  createTempleCard(temples.filter(temple => new Date(temple.dedicated) < new Date('1950-01-01')));
-});
+// document.querySelector('#all').addEventListener('click', () => {
+//   toggleActive(document.querySelector('#all'));
+//   createTempleCard(temples);
+// });
 
-document.querySelector('#new').addEventListener('click', () => {
-  toggleActive(document.querySelector('#new'));
-  createTempleCard(temples.filter(temple => new Date(temple.dedicated) >= new Date('1950-01-01')));
-});
+// document.querySelector('#old').addEventListener('click', () => {
+//   toggleActive(document.querySelector('#old'));
+//   createTempleCard(temples.filter(temple => new Date(temple.dedicated) < new Date('1950-01-01')));
+// });
 
-document.querySelector('#large').addEventListener('click', () => {
-  toggleActive(document.querySelector('#large'));
-  createTempleCard(temples.filter(temple => temple.area >= 50000));
-});
+// document.querySelector('#new').addEventListener('click', () => {
+//   toggleActive(document.querySelector('#new'));
+//   createTempleCard(temples.filter(temple => new Date(temple.dedicated) >= new Date('1950-01-01')));
+// });
 
-document.querySelector('#small').addEventListener('click', () => {
-  toggleActive(document.querySelector('#small'));
-  createTempleCard(temples.filter(temple => temple.area < 50000));
-});
+// document.querySelector('#large').addEventListener('click', () => {
+//   toggleActive(document.querySelector('#large'));
+//   createTempleCard(temples.filter(temple => temple.area >= 50000));
+// });
 
+// document.querySelector('#small').addEventListener('click', () => {
+//   toggleActive(document.querySelector('#small'));
+//   createTempleCard(temples.filter(temple => temple.area < 50000));
+// });
+
+const cutoffDate = new Date('1950-01-01');
+const largeArea = 50000;
 
 const temples = [
   {
     templeName: "Aba Nigéria",
     location: "Aba, Nigéria",
-    dedicated: "2005, Agosto, 7",
+    dedicated: "2005-05-22",
     area: 11500,
     imageUrl:
       "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
@@ -57,7 +60,7 @@ const temples = [
   {
     templeName: "Manti Utah",
     location: "Manti, Utah, Estados Unidos",
-    dedicated: "1888, Maio, 21",
+    dedicated: "1888-05-21",
     area: 74792,
     imageUrl:
       "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
@@ -65,7 +68,7 @@ const temples = [
   {
     templeName: "Payson Utah",
     location: "Payson, Utah, Estados Unidos",
-    dedicated: "2015, Junho, 7",
+    dedicated: "2015-03-15",
     area: 96630,
     imageUrl:
       "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
@@ -73,7 +76,7 @@ const temples = [
   {
     templeName: "Yigo Guam",
     location: "Yigo, Guam",
-    dedicated: "2020, Maio, 2",
+    dedicated: "2020-05-17",
     area: 6861,
     imageUrl:
       "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
@@ -81,7 +84,7 @@ const temples = [
   {
     templeName: "Washington D.C.",
     location: "Kensington, Maryland, Estados Unidos",
-    dedicated: "1974, Novembro, 19",
+    dedicated: "1974-11-19",
     area: 156558,
     imageUrl:
       "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
@@ -89,7 +92,7 @@ const temples = [
   {
     templeName: "Lima Peru",
     location: "Lima, Peru",
-    dedicated: "1986, Janeiro, 10",
+    dedicated: "1986-01-10",
     area: 9600,
     imageUrl:
       "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
@@ -97,13 +100,31 @@ const temples = [
   {
     templeName: "Cidade do México",
     location: "Cidade do México, México",
-    dedicated: "1983, Dezembro, 2",
+    dedicated: "1983-12-02",
     area: 116642,
     imageUrl:
       "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
   }
 
 ];
+
+function setFiler(seletor, filterFunction) {
+  const element = document.querySelector(seletor);
+
+  element.addEventListener('click', () => {
+    toggleActive(element);
+    createTempleCard(temples.filter(filterFunction));
+  });
+}
+
+setFiler('#all', temples);
+setFiler('#old', temple => new Date(temple.dedicated) < cutoffDate);
+setFiler('#new', temple => new Date(temple.dedicated) >= cutoffDate);
+setFiler('#large', temple => temple.area >= largeArea);
+setFiler('#small', temple => temple.area < largeArea);
+
+
+
 
 createTempleCard(temples);
 
